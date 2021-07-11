c1037190d8cfe1dc5062b74f9d02c5df20fe345e
Nix Nix <Nix Nix@DESKTOP-ROU7M8F>
2021-07-11 17:29:13 +0700

    46

--- 1911061930_Lekhang_bigschool/Controllers/AttendancesController.cs
+++ 1911061930_Lekhang_bigschool/Controllers/AttendancesController.cs
@@ -1,4 +1,5 @@
-﻿using _1911061930_Lekhang_bigschool.Models;
+﻿using _1911061930_Lekhang_bigschool.DTOs;
+using _1911061930_Lekhang_bigschool.Models;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
@@ -16,14 +17,14 @@ namespace _1911061930_Lekhang_bigschool.Controllers
             _dbContext = new ApplicationDbContext();
         }
         [HttpPost]
-        public IHttpActionResult Attend([FromBody] int courseId)
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
         {
             var userId = User.Identity.GetUserId();
-            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourserId == courseId))
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && attendanceDto.courseId))
                 return BadRequest("The Attendance Already Exists!");     
             var attendance = new Attendance
             {
-                CourserId = courseId,
+                CourserId = attendanceDto.CourseId,
                 AttendeeId = userId
             };
             _dbContext.Attendances.Add(attendance);
---
+++ 1911061930_Lekhang_bigschool/DTOs/AttendanceDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _1911061930_Lekhang_bigschool.DTOs
+{
+    public class AttendanceDto
+    {
+        public int CourseId { get; set; }
+    }
+}
