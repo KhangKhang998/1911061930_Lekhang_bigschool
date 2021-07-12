16eeec6b047ccdc4d07e41f9e29aa755cb11d3f8
Nix Nix <Nix Nix@DESKTOP-ROU7M8F>
2021-07-12 19:54:52 +0700

    Trang 60 nhe nhang

---
+++ 1911061930_Lekhang_bigschool/Migrations/202107121246160_AddIsCanceledColumnToCourse.cs
@@ -0,0 +1,18 @@
+namespace _1911061930_Lekhang_bigschool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddIsCanceledColumnToCourse : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Courses", "IsCanceled", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Courses", "IsCanceled");
+        }
+    }
+}
--- 1911061930_Lekhang_bigschool/Models/Course.cs
+++ 1911061930_Lekhang_bigschool/Models/Course.cs
@@ -9,6 +9,7 @@ namespace _1911061930_Lekhang_bigschool.Models
     public class Course
     {
         public int Id { get; set; }
+        public bool IsCanceled { get; set; }
         public ApplicationUser Lecturer { get; set; }
         [Required]
         public string LecturerId { get; set; }
